55b60d13cedcfd9ec31879af9d159098e963a668
Gulam Ali <gulam2275@gmail.com>
2024-07-01 21:22:11 +0530

    initial commit

---
+++ Frameworks-SwiftUI/Frameworks_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Frameworks_SwiftUIApp.swift
+//  Frameworks-SwiftUI
+//
+//  Created by Gulam Ali on 01/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Frameworks_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
